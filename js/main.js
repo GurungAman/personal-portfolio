7d99147d52cd69483c61276cb3eaecb8d5c70716
Aman Gurung <amenag30@gmail.com>
2021-07-09 18:07:33 +0545

    aos bug fixed and completed navbar

--- js/main.js
+++ js/main.js
@@ -1,8 +1,37 @@
-var btn = $('#back-to-top');
-$(window).scroll(function() {
+var btn = $("#back-to-top");
+$(window).scroll(function () {
   if ($(window).scrollTop() > 200) {
-    btn.fadeIn('slow');
+    btn.fadeIn("slow");
   } else {
-    btn.fadeOut('slow');
+    btn.fadeOut("slow");
   }
+});
+
+$(function () {
+  $(window).scroll(function () {
+    let scroll = $(window).scrollTop();
+    let navbar = $("#primary-navbar");
+    let hover_effect = $("[id=hover-effect]");
+
+    if (scroll > 100) {
+      navbar.addClass("scrolled-navbar");
+
+      if (parseInt($(window).width()) < 991.98) {
+        hover_effect.addClass("nav-link");
+      } else {
+        hover_effect.removeClass("nav-link");
+
+        hover_effect.addClass("hover-effect");
+      }
+
+      // log_in_button.addClass('scrolled-log-in-button');
+    } else {
+      navbar.removeClass("scrolled-navbar");
+
+      hover_effect.addClass("nav-link");
+      hover_effect.removeClass("hover-effect");
+
+      // log_in_button.removeClass('scrolled-log-in-button');
+    }
+  });
 });
