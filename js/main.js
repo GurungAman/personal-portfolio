b2c1e1d4f15a094d23f3cb60b4d65e0b71fc7ff2
Aman Gurung <amenag30@gmail.com>
2021-07-11 13:30:42 +0545

    some changes to projects section

--- js/main.js
+++ js/main.js
@@ -1,34 +1,42 @@
 var btn = $("#back-to-top");
-$(window).scroll(function () {
-  if ($(window).scrollTop() > 200) {
-    btn.fadeIn("slow");
-  } else {
-    btn.fadeOut("slow");
-  }
+$(window).scroll(function() {
+    if ($(window).scrollTop() > 200) {
+        btn.fadeIn("slow");
+    } else {
+        btn.fadeOut("slow");
+    }
 });
 
-$(function () {
-  $(window).scroll(function () {
-    let scroll = $(window).scrollTop();
-    let navbar = $("#primary-navbar");
-    let hover_effect = $("[id=hover-effect]");
+$(function() {
+    $(window).scroll(function() {
+        let scroll = $(window).scrollTop();
+        let navbar = $("#primary-navbar");
 
-    if (scroll > 100) {
-      navbar.addClass("scrolled-navbar");
+        let hover_effect = $("#primary-navbar .navbar-nav a ");
 
-      if (parseInt($(window).width()) < 991.98) {
-        hover_effect.addClass("nav-link");
-      } else {
-        hover_effect.removeClass("nav-link");
+        if (scroll > 100) {
+            navbar.addClass("scrolled-navbar");
 
-        hover_effect.addClass("hover-effect");
-      }
-    } else {
-      navbar.removeClass("scrolled-navbar");
+            if (parseInt($(window).width()) < 991.98) {
+                hover_effect.addClass("nav-link");
+            } else {
+                hover_effect.removeClass("nav-link");
 
-      hover_effect.addClass("nav-link");
-      hover_effect.removeClass("hover-effect");
+                hover_effect.addClass("hover-effect");
+            }
+        } else {
+            navbar.removeClass("scrolled-navbar");
 
+            hover_effect.addClass("nav-link");
+            hover_effect.removeClass("hover-effect");
+        }
+    });
+});
+/// When you click everywhere in the document
+$(document).click(function(event) {
+    /// If *navbar-collapse* is not among targets of event
+    if (!$(event.target).is(".navbar-collapse *")) {
+        /// Collapse every *navbar-collapse*
+        $(".navbar-collapse").collapse("hide");
     }
-  });
 });
