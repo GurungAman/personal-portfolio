e6520c25bbf924aac42f2c69b1192fcc73d1e126
Aman Gurung <amenag30@gmail.com>
2021-11-16 15:28:13 +0545

    fixed icon jump on initial load

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
   },
   plugins: [
     "gatsby-plugin-styled-components",
+    "gatsby-plugin-fontawesome-css",
     "gatsby-plugin-sass",
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
